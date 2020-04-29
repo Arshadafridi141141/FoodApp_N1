adc390cbb0f5e0bb9f7da7398db2101cb09b39d1
Arshad <arshadafridi141141@gmail.com>
2020-04-29 18:24:43 +0500

    Detail food Activities

--- app/src/main/java/com/example/foodorder/Customer/Fooddetails.java
+++ app/src/main/java/com/example/foodorder/Customer/Fooddetails.java
@@ -40,6 +40,7 @@ public class Fooddetails extends AppCompatActivity {
         cart_button=(FloatingActionButton)findViewById(R.id.btnCart);
         Log.e("checking","in food details");
         Log.e("checking","in food details");
+        Log.e("checking","in food details");
 
         foodname=(TextView)findViewById(R.id.food_name);
         foodprice=(TextView)findViewById(R.id.food_price);
